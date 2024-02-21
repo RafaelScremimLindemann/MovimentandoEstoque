64e9f152b9e8bd8a1f3550b25a62bbc60e504b42
RafaelScremimLindemann <62905043+RafaelScremimLindemann@users.noreply.github.com>
2024-02-20 22:02:44 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo
+{
+    internal class Class1
+    {
+       public string Nome;
+       public double Preco;
+       public int Quantidade;
+
+        public Class1(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public Class1(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade= 0;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade )
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                +Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Estudo;
+using System.Globalization;
+
+/*Programa onde inserimos os dados do produto, chamando a classe pelo objeto.
+  Temos a opção de adicionar um produto e remover, atualizando o estoque.*/
+
+class principal
+{
+    static void Main(string[] args) {
+
+        
+        Console.WriteLine("Dados do produto.");
+        Console.WriteLine("Nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Preço: ");
+        double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Quantidade: ");
+        int quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Class1 p = new Class1(nome, preco, quantidade);
+
+        Console.WriteLine("Estoque atualizado: " + p);
+
+        Console.WriteLine();
+        Console.WriteLine("Digite o numero de produtos a ser adicionado: ");
+        int qte = int.Parse(Console.ReadLine());
+        p.AdicionarProdutos(qte);
+        Console.WriteLine("Estoque Atualizado: " + p);
+
+        Console.WriteLine();
+        Console.WriteLine("Digite o numero de produtos a ser removido: ");
+        qte = int.Parse(Console.ReadLine());
+        p.RemoverProdutos(qte);
+        Console.WriteLine("Estoque Atualizado após remoção: " + p);
+
+    }
+}
